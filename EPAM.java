0f3cf14857b2856801bff81643600cd35915c312
lenakl <47863132+lenakl@users.noreply.github.com>
2019-02-21 18:42:51 +0200

    Add files via upload

---
+++ EPAM.java
@@ -0,0 +1,9 @@
+public class EPAM { 
+  public static void main(String[] args) { 
+    System.out.println("****  ****      *      *     *\n"); 
+    System.out.println("*     *  *     * *     **   **\n");
+    System.out.println("****  ****    *   *    *  *  *\n");
+    System.out.println("*     *      * * * *   *     *\n");
+    System.out.println("****  *     *       *  *     *\n");
+      } 
+} 
