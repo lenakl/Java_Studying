f82834cf276b124ba0114cde53d14db130057a2e
lenakl <47863132+lenakl@users.noreply.github.com>
2019-02-21 21:27:33 +0200

    Delete EPAM.java

--- EPAM.java
+++
@@ -1,9 +0,0 @@
-public class EPAM { 
-  public static void main(String[] args) { 
-    System.out.println("****  ****      *      *     *\n"); 
-    System.out.println("*     *  *     * *     **   **\n");
-    System.out.println("****  ****    *   *    *  *  *\n");
-    System.out.println("*     *      * * * *   *     *\n");
-    System.out.println("****  *     *       *  *     *\n");
-      } 
-} 
